b22a1990aa401be4972a3222ef6b833c97a64590
cnpeete <cpeete@uwyo.edu>
2025-03-01 17:05:54 -0700

    commit

--- wyls.c
+++ wyls.c
@@ -42,12 +42,12 @@ int main(int argc, char **argv)
 
   if (argc == 1)
   {
-    printOutput(argv[0], nFlag, hFlag);
+    printOutput(".", nFlag, hFlag);
   }
   else
   {
-    //only check argv[1] if it exists
-    if (argc > 1)
+    //check to see if argv[2] exists
+    if (argc > 2)
     {
       if (strcmp(argv[1], "-n") == 0 || strcmp(argv[1], "-n -h") == 0 ||
           strcmp(argv[1], "-nh") == 0 || strcmp(argv[1], "-hn") == 0)
@@ -55,30 +55,25 @@ int main(int argc, char **argv)
         nFlag = true;
         printf("nFlag: %d\n", nFlag);
       }
+
       if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-h -n") == 0 ||
           strcmp(argv[1], "-hn") == 0 || strcmp(argv[1], "-nh") == 0)
       {
         hFlag = true;
         printf("hFlag: %d\n", hFlag);
       }
-      //if the first argument triggers neither of thse flags, then maybe we should do the while loop
-      //and call printOutput()
-      if (!nFlag && !hFlag)
+
+      if (!nFlag && !hFlag) //there's no option in position 1
       {
         count = 1;
         while (count < argc)
-        {
+       {
           printOutput(argv[count], nFlag, hFlag);
-          count = count+1;
-        }
+          count = count + 1;
+       }
       }
-    }
-    //we only want to check the 2nd item for a flag if the first entry did not set one of the flags
-    //otherwise we want to call printOutput function
-    if (nFlag || hFlag)
-    {
-      //only check argv[2] if it exists
-      if (argc > 2)
+
+      else //check for an option in 2nd position
       {
         if (strcmp(argv[2], "-n") == 0 || strcmp(argv[2], "-n -h") == 0 ||
             strcmp(argv[2], "-nh") == 0 || strcmp(argv[2], "-hn") == 0)
@@ -92,13 +87,78 @@ int main(int argc, char **argv)
           hFlag = true;
           printf("hFlag: %d\n", hFlag);
         }
-        //put the while loop here since we have evaluated for both flags
-        //we know nFlag or hFlag are true
-        //call the printOutput() function and pass it the values
-        if (nFlag && hFlag)
+      }
+
+      if (nFlag && hFlag && argc == 3)
+      {
+        count = 3;
+        printOutput(".",nFlag,hFlag);
+      }
+      else if (hFlag && nFlag)
+      {
+        count = 3;
+        while (count < argc)
         {
-          count = 3; //have to account for both flags being set
-          //while (count < argc)
+          printOutput(argv[count],nFlag,hFlag);
+          count = count + 1;
+        }
+      }
+      else if (hFlag || nFlag)
+      {
+        count = 2;
+        while (count < argc)
+        {
+          printOutput(argv[count], nFlag,hFlag);
+          count = count + 1;
+        }
+      }
+    }
+    else if (argc > 1)
+    {
+      if (strcmp(argv[1], "-n") == 0 || strcmp(argv[1], "-n -h") == 0 ||
+          strcmp(argv[1], "-nh") == 0 || strcmp(argv[1], "-hn") == 0)
+      {
+        nFlag = true;
+        printf("nFlag: %d\n", nFlag);
+      }
+
+      if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-h -n") == 0 ||
+          strcmp(argv[1], "-hn") == 0 || strcmp(argv[1], "-nh") == 0)
+      {
+        hFlag = true;
+        printf("hFlag: %d\n", hFlag);
+      }
+
+      if (!nFlag && !hFlag) //there's no option in position 1
+      {
+        count = 1;
+        while (count < argc)
+       {
+          printOutput(argv[count], nFlag, hFlag);
+          count = count + 1;
+       }
+      }
+
+      if (nFlag || hFlag && argc == 2)
+      {
+        printOutput(".",nFlag,hFlag);
+      }
+      else if (hFlag && nFlag)
+      {
+        count = 2;
+        while (count < argc)
+        {
+          printOutput(argv[count],nFlag,hFlag);
+          count = count + 1;
+        }
+      }
+      else if (hFlag || nFlag)
+      {
+        count = 2;
+        while (count < argc)
+        {
+          printOutput(argv[count], nFlag,hFlag);
+          count = count + 1;
         }
       }
     }
@@ -131,9 +191,9 @@ void printOutput(char *input, bool nFlag, bool hFlag)
           if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0) continue;
 
           char path[1024];
-          snprintf(path, 1024, "%s%s", input, dp->d_name);
+          snprintf(path, 1024, "%s/%s", input, dp->d_name);
 
-          //if (stat(path, &statbuf) == -1) perror("\nerror");
+          if (stat(path, &statbuf) == -1) perror("\nerror");
           printf("%10.10s", perms(statbuf.st_mode));
 
           if ((pwd = getpwuid(statbuf.st_uid)) != NULL)
@@ -159,6 +219,33 @@ void printOutput(char *input, bool nFlag, bool hFlag)
           printf(" %s %s\n", date, dp->d_name);
         }
       }
+      closedir(dir);
+    }
+    else if (S_ISREG(statbuf.st_mode))
+    {
+      printf("%10.10s", perms(statbuf.st_mode));
+
+      if ((pwd = getpwuid(statbuf.st_uid)) != NULL)
+      {
+        printf(" %-8.8s", pwd->pw_name);
+      }
+      else
+      {
+        printf(" %-8d", statbuf.st_uid);
+      }
+      if ((grp = getgrgid(statbuf.st_gid)) != NULL)
+      {
+        printf(" %8.8s", grp->gr_name);
+      }
+      else
+      {
+        printf(" %-8d", statbuf.st_gid);
+      }
+      printf(" %9jd", (intmax_t)statbuf.st_size);
+
+      tm = localtime(&statbuf.st_mtime);
+      strftime(date, 256, nl_langinfo(D_T_FMT), tm);
+      printf(" %s %s\n", date, input);
     }
 }
 
